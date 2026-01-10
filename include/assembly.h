413e35674161ed0b41175266626e1bcf0b22e9bb
Noah Richmond <noah23rich@outlook.com>
2026-01-10 17:39:02 -0600

    My info

--- include/assembly.h
+++ include/assembly.h
@@ -4,30 +4,27 @@
 
 using namespace vex;
 
-enum lift_positions : int { IDLE = 229, LOADING = 206, SCORING = 99 };
-
 class Assembly {
 public:
     Assembly(
-        mik::motor_group lift_arm_motors,
-        mik::motor intake_motor, 
-        vex::rotation lift_arm_encoder,
-        mik::piston long_piston
+        mik::motor intakeMotor,
+        mik::motor scoringMotor,
+        mik::piston middleGoalPiston,
+        mik::piston armPiston
     );
-    
+
     void init();
     void control();
 
-    void move_lift_arm();
-    void lift_arm_control();
+    void intake_motor_control();
+    void scoring_motor_control();
     void intake_motors_control();
-    void long_piston_control();
+    void middle_goal_piston_control();
+    void arm_piston_control();
 
-    int lift_arm_position = IDLE;
-    vex::task lift_task;
-    
-    mik::motor_group lift_arm_motors;
-    mik::motor intake_motor;
-    vex::rotation lift_arm_encoder;
-    mik::piston long_piston;
+    // Hardware
+    mik::motor intakeMotor;
+    mik::motor scoringMotor;
+    mik::piston middleGoalPiston;
+    mik::piston armPiston;
 };
